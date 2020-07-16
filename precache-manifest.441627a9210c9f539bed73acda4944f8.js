09d202bbe0188ae0f966ede5bf22488171761b7f
Binh 🌱 <binhrobles@gmail.com>
2020-07-16 16:22:16 -0700

    Updates

---
+++ precache-manifest.441627a9210c9f539bed73acda4944f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daea62e5033b0087dac5cd5ffba48874",
+    "url": "/spotify-seasons-client/index.html"
+  },
+  {
+    "revision": "a1ef521b05e73f0b49d7",
+    "url": "/spotify-seasons-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c344909920e6b42d85f",
+    "url": "/spotify-seasons-client/static/js/2.d11a0a86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spotify-seasons-client/static/js/2.d11a0a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ef521b05e73f0b49d7",
+    "url": "/spotify-seasons-client/static/js/main.cf8daeaf.chunk.js"
+  },
+  {
+    "revision": "d7edda9d0d8a34a14cf5",
+    "url": "/spotify-seasons-client/static/js/runtime-main.183d78ae.js"
+  }
+]);
